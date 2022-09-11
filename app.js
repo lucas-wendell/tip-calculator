0a4d25c7dc9e0f46529ff26c96a1eb50f1100776
Lucas Wendell <lucaswhendellf@gmail.com>
2022-09-11 16:44:07 -0300

    refactoring 2.0

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 const numberOfPeopleInput = document.querySelector(
 	'[data-js="numberOfPeopleInput"]'
 );
-const billInput = document.querySelector('[data-js="billInput"]');
-const erroDiv = document.querySelector('[data-js="erroDiv"]');
-
-const closeBtn = document.querySelector('[data-js="closeBtn"]');
-const tipButton = [...document.querySelectorAll('[data-js="tipButton"]')];
-
-const fieldsetSelectTip = document.querySelector(
-	'[data-js="fieldsetSelectTip"]'
-);
-const form = document.querySelector('[data-js="form"]');
 const tipAmountParagrph = document.querySelector(
 	'[data-js="tipAmountParagrph"]'
 );
@@ -18,8 +8,15 @@ const totalValueParagraph = document.querySelector(
 	'[data-js="totalValueParagraph"]'
 );
 
+const billInput = document.querySelector('[data-js="billInput"]');
+const erroDiv = document.querySelector('[data-js="erroDiv"]');
+const tipButtons = [...document.querySelectorAll('[data-js="tipButton"]')];
+
 const tipInput = document.querySelector('[data-js="tipInput"]');
 const resetButton = document.querySelector('[data-js="resetButton"]');
+const form = document.querySelector('[data-js="form"]');
+
+const main = document.querySelector('.main');
 
 const parametersToCaculateTip = {
 	bill: 0,
@@ -27,69 +24,30 @@ const parametersToCaculateTip = {
 	numberOfPeople: 1,
 };
 
-const actions = {
-	throwError(target) {
-		if (target.value < 0) {
-			target.blur();
-			reset();
-			return showDivError(target);
-		}
-	},
-
-	atualizarParametroDeCalculo(property, value) {
-		parametersToCaculateTip[property] = Number(value);
-		calculateTip(parametersToCaculateTip);
-	},
-
-	button(target) {
-		if (target.getAttribute("checked") === "true") {
-			target.setAttribute("checked", "false");
-		} else {
-			tipButton.forEach(button => {
-				if (button.value !== target.value) {
-					button.setAttribute("checked", "false");
-				}
-			});
-			tipInput.value = "";
-			parametersToCaculateTip.tipPercentage = Number(target.value);
-
-			calculateTip(parametersToCaculateTip);
-			target.setAttribute("checked", "true");
-		}
-	},
-
-	bill(target) {
-		this.throwError(target);
-		if (target.value !== "") {
-			resetButton.removeAttribute("disabled");
-		}
-
-		this.atualizarParametroDeCalculo(target.name, target.value);
-	},
-
-	numberOfPeople(target) {
-		this.throwError(target);
-
-		this.atualizarParametroDeCalculo(
-			target.name,
-			Number(target.value === "" ? 1 : target.value)
-		);
-	},
-
-	tipPercentage(target) {
-		this.throwError(target);
-		tipButton.forEach(button => button.setAttribute("checked", "false"));
-
-		this.atualizarParametroDeCalculo(
-			target.name,
-			Number(target.value === "" ? 1 : target.value)
-		);
-	},
-};
-
 const addCalculationValueInDOM = (tipAmount, total) => {
 	tipAmountParagrph.textContent = `$${tipAmount}`;
 	totalValueParagraph.textContent = `$${total}`;
+};
+
+const deselectButtons = () =>
+	tipButtons.forEach(button => button.setAttribute('checked', 'false'));
+
+const reset = ({ bill, tipPercentage, numberOfPeople }) => {
+	const zero = Number(0).toFixed(2);
+	resetButton.setAttribute('disabled', 'true');
+
+	billInput.value = '';
+	numberOfPeopleInput.value = '';
+	tipInput.value = '';
+
+	deselectButtons();
+	const [button] = tipButtons.filter(button => button.value === '5');
+	button.setAttribute('checked', 'true');
+
+	bill = 0;
+	tipPercentage = 5;
+	numberOfPeople = 1;
+	addCalculationValueInDOM(zero, zero);
 };
 
 const calculateTip = ({ bill, tipPercentage, numberOfPeople }) => {
@@ -99,39 +57,80 @@ const calculateTip = ({ bill, tipPercentage, numberOfPeople }) => {
 	addCalculationValueInDOM(tipAmount.toFixed(2), total.toFixed(2));
 };
 
-const reset = () => {
-	const zero = Number(0).toFixed(2);
-	resetButton.setAttribute("disabled", "true");
+const actions = {
+	closeBtn() {
+		billInput.focus();
+		erroDiv.style.zIndex = '-1';
+		erroDiv.style.top = '0px';
+	},
 
-	billInput.value = "";
-	numberOfPeopleInput.value = "";
-	tipInput.value = "";
+	showDivError(target) {
+		erroDiv.style.top = '-80px';
+		erroDiv.style.zIndex = '0';
 
-	tipButton.forEach(button => {
-		if (button.value === "5") {
-			button.setAttribute("checked", "true");
-		} else if (button.value !== "5") {
-			button.setAttribute("checked", "false");
+		erroDiv.style.visibility = 'visible';
+		target.value = '';
+	},
+
+	throwError(target) {
+		if (target.value < 0) {
+			target.blur();
+			this.showDivError(target);
+			return true;
 		}
-	});
-	parametersToCaculateTip.bill = 0;
-	parametersToCaculateTip.tipPercentage = 5;
-	parametersToCaculateTip.numberOfPeople = 1;
-	addCalculationValueInDOM(zero, zero);
-};
+	},
 
-const showDivError = target => {
-	erroDiv.style.top = "-80px";
-	erroDiv.style.zIndex = "0";
+	updateCalculationParameters(property, value) {
+		parametersToCaculateTip[property] = Number(value);
+		calculateTip(parametersToCaculateTip);
+	},
 
-	erroDiv.style.visibility = "visible";
-	target.value = "";
-};
+	tipButton(target) {
+		deselectButtons();
+		tipInput.value = '';
 
-const hideDivError = () => {
-	billInput.focus();
-	erroDiv.style.zIndex = "-1";
-	erroDiv.style.top = "0px";
+		this.updateCalculationParameters(
+			target.getAttribute('property'),
+			target.value
+		);
+
+		target.setAttribute('checked', 'true');
+	},
+
+	billInput(target) {
+		if (this.throwError(target)) return;
+
+		const property = target.getAttribute('property');
+		const value = target.value;
+
+		if (target.value !== '') {
+			resetButton.removeAttribute('disabled');
+		}
+
+		this.updateCalculationParameters(property, value);
+	},
+
+	numberOfPeopleInput(target) {
+		if (this.throwError(target)) return;
+
+		const property = target.getAttribute('property');
+		const value = Number(target.value === '' ? 1 : target.value);
+		this.updateCalculationParameters(property, value);
+	},
+
+	tipCustom(target) {
+		if (this.throwError(target)) return;
+		deselectButtons();
+
+		this.updateCalculationParameters(
+			target.property,
+			Number(target.value === '' ? 1 : target.value)
+		);
+	},
+
+	resetButton() {
+		reset(parametersToCaculateTip);
+	},
 };
 
 const accessFunction = (target, funcitonName) => {
@@ -139,29 +138,17 @@ const accessFunction = (target, funcitonName) => {
 	func?.call(actions, target);
 };
 
-tipInput.addEventListener("input", e =>
+tipInput.addEventListener('input', e =>
 	accessFunction(e.target, e.target.name)
 );
 
-fieldsetSelectTip.addEventListener("click", e =>
+numberOfPeopleInput.addEventListener('input', e =>
 	accessFunction(e.target, e.target.name)
 );
 
-numberOfPeopleInput.addEventListener("input", e =>
+billInput.addEventListener('input', e =>
 	accessFunction(e.target, e.target.name)
 );
 
-billInput.addEventListener("input", e =>
-	accessFunction(e.target, e.target.name)
-);
-
-closeBtn.addEventListener("click", hideDivError);
-resetButton.addEventListener("click", reset);
-form.addEventListener("submit", e => e.preventDefault());
-
-/*
-	- feita! (Fazer função que calcula a gorjeta)
-	- feita! (Criar função que vai colocar checked nos butões)
-	- feita! (Criar função de reset)
-	- Refatorar
-*/
+form.addEventListener('submit', e => e.preventDefault());
+main.addEventListener('click', e => accessFunction(e.target, e.target.name));
